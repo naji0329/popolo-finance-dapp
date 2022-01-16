55c6d063c4a519edd4527c824672b7cf0dbe88b9
NAJI2329 <be1512t6@gmail.com>
2022-01-16 19:04:36 +0900

    2022.01.16 NAJI Log view change

--- src/views/Home.js
+++ src/views/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
     <>
       <Hero className="illustration-section-01" />
       <FeaturesTiles />
-      {/* <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" /> */}
+      <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
       <Testimonial topDivider />
       <Cta split />
     </>
